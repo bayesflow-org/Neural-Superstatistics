00faf20e1ed9fc392e4c0abc2490bfd57bdb1054
stefanradev93 <stefan.radev93@gmail.com>
2023-06-14 16:23:42 +0000

    Prepare changes

--- applications/coal_mining/configuration.py
+++ applications/coal_mining/configuration.py
@@ -1,12 +1,13 @@
 default_settings = {
-    "gru_hidden_units": 512,
-    "lstm1_hidden_units": 512,
-    "lstm2_hidden_units": 512,
-    "hidden_units_local": 256,
-    "hidden_units_global": 256,
+    "gru_hidden_units": 128,
+    "lstm1_hidden_units": 128,
+    "lstm2_hidden_units": 128,
+    "hidden_units_local": 128,
+    "hidden_units_global": 128,
     "trainer": {
         "checkpoint_path": "../checkpoints/coal_mining",
         "max_to_keep": 1,
+        "default_lr": 1e-4,
         "memory": False,
     },
 }
