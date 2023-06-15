3af0d3d4076f5ba17fe101dc613a10907ff147c5
Lukas Schumacher <39727780+LuSchumacher@users.noreply.github.com>
2023-06-15 17:21:12 +0200

    add priors and likelihoods

---
+++ applications/optimal_policy/likelihoods.py
@@ -0,0 +1,122 @@
+import numpy as np
+from numba import njit
+
+@njit
+def _sample_diffusion_trial(v, a, tau, beta=0.5, dt=0.001, s=1.0, max_iter=1e5):
+    """Generates a single response time from a Diffusion Decision process.
+
+    Parameters:
+    -----------
+    v        : float
+        The drift rate parameter.
+    a        : float
+        The boundary separation parameter.
+    tau      : float
+        The non-decision time parameter.
+    beta     : float, optional, default: 0.5
+        The starting point parameter. The default corresponds to
+        no a priori bias.
+    dt       : float, optional, default: 0.001
+        Time resolution of the process. Default corresponds to
+        a precision of 1 millisecond.
+    s        : float, optional, default: 1
+        Scaling factor of the Wiener noise.
+    max_iter : int, optional, default: 1e5
+        Maximum iterations of the process. Default corresponds to
+        100 seconds.
+
+    Returns:
+    --------
+    rt : flaot
+        A response time samples from the static Diffusion decision process.
+        Reaching the lower boundary results in a negative rt.
+    """
+    n_iter = 0
+    x = a * beta
+    c = np.sqrt(dt * s)
+    while x > 0 and x < a and n_iter < max_iter:
+        x += v*dt + c * np.random.randn()
+        n_iter += 1
+    rt = n_iter * dt
+    return rt+tau if x >= 0 else -(rt+tau)
+
+@njit
+def sample_static_diffusion_process(theta, num_steps=1320, beta=0.5, dt=0.001, s=1.0, max_iter=1e5):
+    """Generates a single simulation from a static
+    Diffusion decision process with a static parameters.
+
+    Parameters:
+    -----------
+    theta     : np.array of shape (3, )
+        The 3 latent DDM parameters, v, a, tau.
+    num_steps : int, optional, default: 1320
+        The number of time steps to take for the random walk. Default
+        corresponds to the maximal number of trials in the Optimal Policy Dataset.
+    beta     : float, optional, default: 0.5
+        The starting point parameter. The default corresponds to
+        no a priori bias.
+    dt       : float, optional, default: 0.001
+        Time resolution of the process. Default corresponds to
+        a precision of 1 millisecond.
+    s        : float, optional, default: 1
+        Scaling factor of the Wiener noise.
+    max_iter : int, optional, default: 1e5
+        Maximum iterations of the process. Default corresponds to
+        100 seconds.
+
+    Returns:
+    --------
+    rt : np.array of shape (num_steps, )
+        Response time samples from the static Diffusion decision process.
+        Reaching the lower boundary results in negative rt's.
+    """
+
+    rt = np.zeros(num_steps)
+    for t in range(num_steps):
+        rt[t] = _sample_diffusion_trial(
+            theta[0], theta[1], theta[2], beta,
+            dt=dt, s=s, max_iter=max_iter)
+    return rt
+
+@njit
+def sample_stationary_diffusion_process(theta_t):
+    pass
+
+@njit
+def sample_random_walk_diffusion_process(theta_t, beta=0.5, dt=0.001, s=1.0, max_iter=1e5):
+    """Generates a single simulation from a non-stationary
+    Diffusion decision process with a parameters following a random walk.
+
+    Parameters:
+    -----------
+    theta_t : np.ndarray of shape (theta_t, 3)
+        The trajectory of the 3 latent DDM parameters, v, a, tau.
+    num_steps : int, optional, default: 1320
+        The number of time steps to take for the random walk. Default
+        corresponds to the maximal number of trials in the Optimal Policy Dataset.
+    beta     : float, optional, default: 0.5
+        The starting point parameter. The default corresponds to
+        no a priori bias.
+    dt       : float, optional, default: 0.001
+        Time resolution of the process. Default corresponds to
+        a precision of 1 millisecond.
+    s        : float, optional, default: 1
+        Scaling factor of the Wiener noise.
+    max_iter : int, optional, default: 1e5
+        Maximum iterations of the process. Default corresponds to
+        100 seconds.
+
+    Returns:
+    --------
+    rt : np.array of shape (num_steps, )
+        Response time samples from the static Diffusion decision process.
+        Reaching the lower boundary results in negative rt's.
+    """
+
+    num_steps = theta_t.shape[0]
+    rt = np.zeros(num_steps)
+    for t in range(num_steps):
+        rt[t] = _sample_diffusion_trial(
+            theta_t[t, 0], theta_t[t, 1], theta_t[t, 2], beta,
+            dt=dt, s=s, max_iter=max_iter)
+    return rt
--- applications/optimal_policy/priors.py
+++ applications/optimal_policy/priors.py
@@ -3,6 +3,9 @@ import numpy as np
 LOWER_BOUNDS = np.array([0, 0, 0])
 UPPER_BOUNDS = np.array([8, 6, 1])
 
+DDM_PARAM_PRIOR_SHAPES = np.array([5.0, 4.0, 1.5])
+DDM_PARAM_PRIOR_SCALES = np.array([1/3.0, 1/3.0, 1/5.0])
+
 def sample_scale(a=1, b=25, num_params=3, rng=None):
     """Generates num_params random draws from a beta prior over the
     scale of the random walk.
@@ -10,18 +13,18 @@ def sample_scale(a=1, b=25, num_params=3, rng=None):
     Parameters:
     -----------
     a          : float, optional, default: 1
-        The first parameter of the beta distribution
+        The first parameter of the beta distribution.
     b          : float, optional, default: 25
-        The second parameter of the beta distribution
+        The second parameter of the beta distribution.
     num_params : int, optional, default: 3
-        The number of scale parameters
+        The number of scale parameters.
     rng        : np.random.Generator or None, default: None
         An optional random number generator to use, if fixing the seed locally.
 
     Returns:
     --------
     scale : np.array
-        The randomly drawn scale parameters
+        The randomly drawn scale parameters.
     """
 
     if rng is None:
@@ -29,29 +32,51 @@ def sample_scale(a=1, b=25, num_params=3, rng=None):
     return rng.beta(a, b, num_params)
 
 def sample_switch_prob(min=0.0, max=0.2, num_params=2, rng=None):
-    """Generates two random draws from a uniform prior over the
+    """Generates num_params random draws from a uniform prior over the
     switch probability of the regime switching transition.
 
     Parameters:
     -----------
     min        : float, optional, default: 0.0
-        The minimum of the uniform distribution
+        The minimum of the uniform distribution.
     max        : float, optional, default: 0.2
-        The maximum of the uniform distribution
+        The maximum of the uniform distribution.
     num_params : int, optional, default: 2
-        The number of switch probability parameters
+        The number of switch probability parameters.
     rng : np.random.Generator or None, default: None
         An optional random number generator to use, if fixing the seed locally.
 
     Returns:
     --------
     switch_prob : np.array
-        The randomly drawn switch probability parameters
+        The randomly drawn switch probability parameters.
     """
     if rng is None:
         rng = np.random.default_rng()
-
     return rng.uniform(min, max, num_params)
+
+def sample_ddm_params(shapes=DDM_PARAM_PRIOR_SHAPES, scales=DDM_PARAM_PRIOR_SCALES, rng=None):
+    """Generates random draws from a gamma prior over the
+    diffusion decision parameters, v, a, tau.
+
+    Parameters:
+    -----------
+    shapes     : np.array, optional, default: [5.0, 4.0, 1.5]
+        The shapes of the gamma distribution.
+    scales     : np.array, optional, default: [1/3.0, 1/3.0, 1/5.0]
+        The scales of the gamma distribution.
+    rng : np.random.Generator or None, default: None
+        An optional random number generator to use, if fixing the seed locally.
+
+    Returns:
+    --------
+    ddm_params : np.array
+        The randomly drawn DDM parameters, v, a, tau.
+    """
+
+    if rng is None:
+        rng = np.random.default_rng()
+    return rng.gamma(shape=shapes, scale=scales)
 
 def sample_random_walk(sigmas, num_steps=1320, lower_bounds=LOWER_BOUNDS, upper_bounds=UPPER_BOUNDS, rng=None):
     """Generates a single simulation from a random walk transition model.
@@ -59,10 +84,10 @@ def sample_random_walk(sigmas, num_steps=1320, lower_bounds=LOWER_BOUNDS, upper_
     Parameters:
     -----------
     sigmas          : np.array
-        The standard deviations of the random walk process
+        The standard deviations of the random walk process.
     num_steps       : int, optional, default: 1320
         The number of time steps to take for the random walk. Default
-        corresponds to the maximal number of trials in the Optimal Policy Dataset
+        corresponds to the maximal number of trials in the Optimal Policy Dataset.
     lower_bounds    : np.array, optional, default: [0, 0, 0]
         The minimum values the parameters can take.
     upper_bound     : np.array, optional, default: [8, 6, 1]
@@ -82,12 +107,10 @@ def sample_random_walk(sigmas, num_steps=1320, lower_bounds=LOWER_BOUNDS, upper_
 
      # Sample initial parameters
     theta_t = np.zeros((num_steps, 3))
-    theta_t[0, 0] = rng.gamma(shape=5.0, scale=1.0/3.0)
-    theta_t[0, 1] = rng.gamma(shape=4.0, scale=1.0/3.0)
-    theta_t[0, 2] = rng.gamma(shape=1.5, scale=1.0/5.0)
+    theta_t[0] = sample_ddm_params()
 
     # Run random walk from initial
-    z = rng.random(size=(num_steps - 1, 3))
+    z = rng.normal(size=(num_steps - 1, 3))
     for t in range(1, num_steps):
         theta_t[t] = np.clip(
             theta_t[t - 1] + sigmas * z[t - 1], lower_bounds, upper_bounds
