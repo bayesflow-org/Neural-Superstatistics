69f73929e5b34be4b113388d4ef74471b90cf19f
Lukas Schumacher <39727780+LuSchumacher@users.noreply.github.com>
2023-06-15 18:12:01 +0200

    add models.py

--- applications/optimal_policy/models.py
+++ applications/optimal_policy/models.py
@@ -1,4 +1,15 @@
 from abc import ABC, abstractmethod
+from functools import partial
+import numpy as np
+import bayesflow as bf
+
+from priors import sample_scale, sample_switch_prob, sample_ddm_params, sample_random_walk
+from likelihoods import sample_static_diffusion_process, sample_stationary_diffusion_process, sample_random_walk_diffusion_process
+
+HYPER_PRIOR_MEAN = 0.04
+HYPER_PRIOR_STD = 0.04
+LOCAL_PRIOR_MEANS = np.array([1.8, 1.5, 0.4])
+LOCAL_PRIOR_STDS = np.array([1.2, 1.0, 0.3])
 
 
 class DiffusionModel(ABC):
@@ -11,22 +22,107 @@ class DiffusionModel(ABC):
     @abstractmethod
     def generate(self, batch_size, *args, **kwargs):
         pass
-    
+
     @abstractmethod
     def configure(self, raw_dict, *args, **kwargs):
         pass
 
 
 class StaticDiffusionModel(DiffusionModel):
+    """A wrapper for a static Diffusion Decision process."""
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, rng=None):
+        """Creates an instance of a static Diffusion Decision Model with given configuration.
+        When used in a BayesFlow pipeline, only the attribute ``self.generator`` and
+        the method ``self.configure`` should be used.
+
+        Parameters:
+        -----------
+        rng : np.random.Generator or None, default: None
+            An optional random number generator to use, if fixing the seed locally.
+        """
+
+        # Store local RNG instance
+        if rng is None:
+            rng = np.random.default_rng()
+        self._rng = rng
+
+        # Create prior wrapper
+        self.prior = bf.simulation.Prior(
+            prior_fun=partial(sample_ddm_params, rng=self._rng),
+        )
+
+        # Create simulator wrapper
+        self.likelihood = bf.simulation.Simulator(
+            simulator_fun=sample_static_diffusion_process,
+        )
+
+        # Create generative model wrapper. Will generate 3D tensors
+        self.generator = bf.simulation.GenerativeModel(
+            prior=self.prior,
+            simulator=self.likelihood,
+            name="static_diffusion_model",
+        )
 
     def generate(self, batch_size, *args, **kwargs):
-        pass
+        """Wraps the call function of ``bf.simulation.GenerativeModel``.
 
-    def configure(self, raw_dict):
-        pass
+        Parameters:
+        -----------
+        batch_size : int
+            The number of simulations to generate per training batch
+        **kwargs   : dict, optional, default: {}
+            Optional keyword arguments passed to the call function of ``bf.simulation.GenerativeModel``
+
+        Returns:
+        --------
+        raw_dict   : dict
+            The simulation dictionary configured for ``bayesflow.amortizers.Amortizer``
+        """
+
+        return self.generator(batch_size, **kwargs)
+
+    def configure(self, raw_dict, transform=True):
+        """Configures the output of self.generator for a BayesFlow pipeline.
+
+        1. Converts float64 to float32 (for TensorFlow)
+        2. Appends a trailing dimensions of 1 to data
+        3. Scale the model parameters
+
+        Parameters:
+        -----------
+        raw_dict  : dict
+            A simulation dictionary as returned by ``bayesflow.simulation.GenerativeModel``
+        transform : boolean,
+            An indicator to standardize the parameters. 
+
+        Returns:
+        --------
+        input_dict : dict
+            The simulation dictionary configured for ``bayesflow.amortizers.Amortizer``
+        """
+
+        # Extract relevant simulation data, convert to float32, and add extra dimension
+        theta = raw_dict.get("prior_draws").astype(np.float32)
+        rt = raw_dict.get("sim_data").astype(np.float32)[..., None]
+
+        if transform:
+            # get prior means and stds for scaling
+            prior_means, prior_stds = self.prior.estimate_means_and_stds(n_draws=10000)
+            prior_means = np.round(prior_means, decimals=1)
+            prior_stds = np.round(prior_stds, decimals=1)
+
+            out_dict = dict(
+                parameters=(theta - prior_means) / prior_stds,
+                summary_conditions=rt,
+            )
+        else:
+            out_dict = dict(
+                parameters=theta,
+                summary_conditions=rt,
+            )
+
+        return out_dict
 
 
 class StationaryDiffusion(DiffusionModel):
@@ -42,15 +138,100 @@ class StationaryDiffusion(DiffusionModel):
 
 
 class RandomWalkDiffusion(DiffusionModel):
+    """A wrapper for a Non-Stationary Diffusion Decision process with
+    a Gaussian random walk transition model."""
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, rng=None):
+        """Creates an instance of the Non-Stationary Diffusion Decision with given configuration.
+        When used in a BayesFlow pipeline, only the attribute ``self.generator`` and
+        the method ``self.configure`` should be used.
+
+        Parameters:
+        -----------
+        rng : np.random.Generator or None, default: None
+            An optional random number generator to use, if fixing the seed locally.
+        """
+
+        # Store local RNG instance
+        if rng is None:
+            rng = np.random.default_rng()
+        self._rng = rng
+
+        # Create prior wrapper
+        self.prior = bf.simulation.TwoLevelPrior(
+            hyper_prior_fun=partial(sample_scale, rng=self._rng),
+            local_prior_fun=partial(sample_random_walk, rng=self._rng),
+        )
+
+        # Create simulator wrapper
+        self.likelihood = bf.simulation.Simulator(
+            simulator_fun=sample_random_walk_diffusion_process,
+        )
+
+        # Create generative model wrapper. Will generate 3D tensors
+        self.generator = bf.simulation.TwoLevelGenerativeModel(
+            prior=self.prior,
+            simulator=self.likelihood,
+            name="random_walk_diffusion_model",
+        )
 
     def generate(self, batch_size, *args, **kwargs):
-        pass
+        """Wraps the call function of ``bf.simulation.TwoLevelGenerativeModel``.
+
+        Parameters:
+        -----------
+        batch_size : int
+            The number of simulations to generate per training batch
+        **kwargs   : dict, optional, default: {}
+            Optional keyword arguments passed to the call function of ``bf.simulation.TwoLevelGenerativeModel``
+
+        Returns:
+        --------
+        raw_dict   : dict
+            The simulation dictionary configured for ``bayesflow.amortizers.TwoLevelAmortizer``
+        """
+
+        return self.generator(batch_size, **kwargs)
 
     def configure(self, raw_dict):
-        pass
+        """Configures the output of self.generator for a BayesFlow pipeline.
+
+        1. Converts float64 to float32 (for TensorFlow)
+        2. Appends a trailing dimensions of 1 to data
+        3. Scale the model parameters if tranform=True
+
+        Parameters:
+        -----------
+        raw_dict  : dict
+            A simulation dictionary as returned by ``bayesflow.simulation.TwoLevelGenerativeModel``
+        transform : boolean,
+            An indicator to standardize the parameter and log-transform the data samples. 
+
+        Returns:
+        --------
+        input_dict : dict
+            The simulation dictionary configured for ``bayesflow.amortizers.TwoLevelAmortizer``
+        """
+
+        # Extract relevant simulation data, convert to float32, and add extra dimension
+        theta_t = raw_dict.get("local_prior_draws").astype(np.float32)
+        scales = raw_dict.get("hyper_prior_draws").astype(np.float32)
+        rt = raw_dict.get("sim_data").astype(np.float32)[..., None]
+
+        if transform:
+            out_dict = dict(
+                local_parameters=(theta_t - LOCAL_PRIOR_MEANS) / LOCAL_PRIOR_STDS,
+                hyper_parameters=(scales - HYPER_PRIOR_MEAN) / HYPER_PRIOR_STD,
+                summary_conditions=rt,
+            )
+        else:
+            out_dict = dict(
+                local_parameters=theta_t,
+                hyper_parameters=scales,
+                summary_conditions=rt
+            )
+
+        return out_dict
 
 
 class RegimeSwitchingDiffusion(DiffusionModel):
