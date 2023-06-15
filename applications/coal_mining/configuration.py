9ae57e8f1c3437129b594f84acbfba2f1b020295
stefanradev93 <stefan.radev93@gmail.com>
2023-06-15 08:02:58 +0000

    Finalize coal mining with buggy bayesloop

--- applications/coal_mining/configuration.py
+++ applications/coal_mining/configuration.py
@@ -1,13 +1,12 @@
 default_settings = {
-    "gru_hidden_units": 128,
-    "lstm1_hidden_units": 128,
-    "lstm2_hidden_units": 128,
-    "hidden_units_local": 128,
-    "hidden_units_global": 128,
+    "lstm1_hidden_units": 512,
+    "lstm2_hidden_units": 512,
+    "hidden_units_local": 256,
+    "hidden_units_global": 256,
     "trainer": {
         "checkpoint_path": "../checkpoints/coal_mining",
         "max_to_keep": 1,
-        "default_lr": 1e-4,
+        "default_lr": 5e-4,
         "memory": False,
     },
 }
--- applications/coal_mining/experiments.py
+++ applications/coal_mining/experiments.py
@@ -40,9 +40,6 @@ class NeuralCoalMiningExperiment:
             [
                 tf.keras.Sequential(
                     [
-                        tf.keras.layers.GRU(
-                            config["gru_hidden_units"], return_sequences=True
-                        ),
                         tf.keras.layers.LSTM(
                             config["lstm1_hidden_units"], return_sequences=True
                         )
